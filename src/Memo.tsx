3b10fc8dd3384c7f8babf80a9e72bff29987cddc
magriker <magken7@gmail.com>
2024-11-24 22:14:52 +0900

    firstcommit

---
+++ src/Memo.tsx
@@ -0,0 +1,8 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import * as React from "react";
+import { Container } from "@mui/material";
+
+const Memo = (): any => {
+  return <></>;
+};
